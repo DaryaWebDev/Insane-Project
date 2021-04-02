dec4106b2f14b7a9a3cf81afe64fd91b887f0094
DaryaWebDev <feral2015@mail.ru>
2021-04-02 18:48:26 +0300

    Added: blog PP and adaptive

--- js/main.js
+++ js/main.js
@@ -28,3 +28,18 @@ const blogSwiper = new Swiper(".blog-posts__swiper", {
   },
 });
 
+
+jQuery(document).ready(function () {
+  var btn = $("#button");
+  $(window).scroll(function () {
+    if ($(window).scrollTop() > 300) {
+      btn.addClass("show");
+    } else {
+      btn.removeClass("show");
+    }
+  });
+  btn.on("click", function (e) {
+    e.preventDefault();
+    $("html, body").animate({ scrollTop: 0 }, "300");
+  });
+});
